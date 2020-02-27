5ff9c1f9ebe46ba73253e41dd57789b392fccca6
Gleb Zimin <gzimin@mirantis.com>
2020-02-27 12:39:58 +0400

    Revert "Move panel group definitions"
    This reverts commit 33c1dc06b8e30046637695fc85fef17b37f8b1d8.

--- contrail_dashboard/enabled/_1432_project_networks_panel.py
+++ contrail_dashboard/enabled/_1432_project_networks_panel.py
@@ -5,8 +5,13 @@ from django.utils.translation import ugettext_lazy as _
 PANEL = 'networks'
 # The slug of the dashboard the PANEL associated with. Required.
 PANEL_DASHBOARD = 'project'
-# The slug of the panel group the PANEL is associated with.
-PANEL_GROUP = 'contrail_network'
-
 # Python panel class of the PANEL to be added.
 ADD_PANEL = 'contrail_dashboard.content.networks.panel.Networks'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'contrail_network'
+PANEL_GROUP_NAME = _('ContrailNetwork')
+
+ADD_INSTALLED_APPS = ["contrail_dashboard", ]
+
+# Automatically discover static resources in installed apps
+AUTO_DISCOVER_STATIC_FILES = True
