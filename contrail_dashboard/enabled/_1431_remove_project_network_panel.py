38648cb9120fe79c0a3622fa90d572b0eee02f3d
Gleb Zimin <gzimin@mirantis.com>
2020-02-27 11:18:03 +0400

    Replace network panel
    We remove old network panel and add new one.
    We need it to fix issue with LBaaSv2 and make
    contrail-horizon-plugin easier.

---
+++ contrail_dashboard/enabled/_1431_remove_project_network_panel.py
@@ -0,0 +1,8 @@
+# The slug of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'networks'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'project'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'network'
+
+REMOVE_PANEL = True
